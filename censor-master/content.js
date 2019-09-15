503a3750dcb3978280a1e2c2973b844d8d631ec2
unknown <frankfrankfrankyeyeye@gmail.com>
2019-09-15 06:53:46 -0400

    Memoization and proxy

--- censor-master/content.js
+++ censor-master/content.js
@@ -1,5 +1,11 @@
 PROXY_URL = "http://localhost:9000/";
+var THRESHOLD;
+chrome.storage.sync.get(['sensitivity'], function(result) {
+        console.log('Sensitivity currently is ' + result.sensitivity);
+        THRESHOLD = result.key;
+    });
 
+VERY_HACKY_MEMOIZATION = {};
 
 window.onload = function () {
         var links = document.getElementsByTagName('a');
@@ -23,14 +29,20 @@ window.onload = function () {
 
 
         function censorTag(j, sentimentData) {
+            console.log("CENSORING " + "j")
             j.style.backgroundColor = "rgb(32, 33, 36)";
             j.style.transition = "all 0.3"; // TODO TEST DURATION
             $(j).parent().hover(function() {$(this).css("background-color", "rgb(32, 33, 36)")}, function() {$(this).css("background-color", "rgb(32, 33, 36)")});
-            // j.setAttribute("onmouseover", "this.style.backgroundColor = null");
-            // j.setAttribute("onmouseout", "this.style.backgroundColor = rgb(32, 33, 36)");
         }
 
         function requestHandler(u, titles) {
+            if (VERY_HACKY_MEMOIZATION[titles[u].innerText]) {
+                console.log("Foud memo");
+                    if ((JSON.parse(VERY_HACKY_MEMOIZATION[titles[u].innerText]).emotion.Angry + JSON.parse(VERY_HACKY_MEMOIZATION[titles[u].innerText]).emotion.Sad + JSON.parse(VERY_HACKY_MEMOIZATION[titles[u].innerText]).emotion.Fear) > THRESHOLD/500) {
+                        censorTag(titles[u], VERY_HACKY_MEMOIZATION[titles[u].innerText]); // SMOL HACK
+                    }
+                    return 1;
+            }
             httpRequests = new XMLHttpRequest();
             httpRequests.open('POST', PROXY_URL + 'https://apis.paralleldots.com/v4/emotion', true);
             httpRequests.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
@@ -39,21 +51,58 @@ window.onload = function () {
                 if (this.status >= 200 && this.status < 400) {
                     console.log("Request " + u + ": " + this.response);
                     console.log(this.response);
-                    if ((JSON.parse(this.response).emotion.Angry + JSON.parse(this.response).emotion.Sad + JSON.parse(this.response).emotion.Fear) > 0.6) {
+                    try {
+                    if ((JSON.parse(this.response).emotion.Angry + JSON.parse(this.response).emotion.Sad + JSON.parse(this.response).emotion.Fear) > THRESHOLD/500) {
+                        VERY_HACKY_MEMOIZATION[titles[u].innerText] = JSON.parse(this.response);
                         censorTag(titles[u], this.response); // SMOL HACK
+                        }
+                    } catch (err) {
+                         return false;
                     }
+
                 }
             }
 
-            var api_keys = ["5pC6m1e0wOLBdkwtOcfzesFReIsbFy5fTDhhJHQoEnQ", "LIjMokIVHOugoE2hZz05YnK4kEnbvWlSq8uSTuVDLhM", "vOI8Ghq3vZa5gMr1aD0Nx5AAtgIglE3DnEm9Emt49PM", "56XXL2sWgeHpLWcGbBfXxPsHVwctP34TnR1C2mgyXmE", "iDFzBUlrvxAUxSRlRBW3CW7yx31VVQZtI1xUrwduqhs", "aDcblb0XeBkQi7WewErefoawIDO7wCFAGDLXSnuZVb4", "hiygXuNBRbXmzCIbl6xKsRxIQkJECdB1VZqE43OYMxs", "p28cU33iBfqwFTJ87DADthrGT4dKtcovS6Ude5GHpGw", "hiYBuZYHjbUxjdLIwzjtfgXwMxhkbbz3K2jzon5CFlM"];
+            var api_keys = ["JqhbglNQANAFrQYZ6HfYmH96vM4u92CyoEnHM4lp8AM", "xGqJ2AWAJ4hw08xWRNKxNAo8NGk3E3NytpgmIayPtdY", "ttlVqZxpXH4Gs6Kg3T0ETA8a8jun25Wj1hD9jrFMx2o", "1bJnRMtoaMqwymXpE98xpbqu4xq2h1FXX7TYiXtyf28", "LhDnEAFDm5jnVcFoF662pCpwxdViOKNOFKXxFUDzZqg",]
             var rand_key = api_keys[u % api_keys.length];  //api_keys[Math.round(Math.random(0, api_keys.length))]
+                httpRequests.send(`text=${titles[u].innerText}&api_key=${rand_key}`);
 
-            httpRequests.send(`text=${titles[u].innerText}&api_key=${rand_key}`);
             console.log("Key: " + rand_key);
         }
-
-        for (i = 0; i < titles.length; i++) {
+ 
+        for (i = 0; i < titles.length; ++i) {
             requestHandler(i, titles);
         }
-
+ 
+        var lastScrollTop = 0;
+        $(window).scroll(function(event) {
+            var st = $(this).scrollTop();
+            if (st > lastScrollTop) {
+                var linksNew = document.getElementsByTagName('a');
+                var titlesNew = [];
+ 
+                for (var k = 0; k < linksNew.length; ++k) {
+                    if(linksNew[k].href.includes("news.google.com/articles")){
+                        if(!linksNew[k].href.includes("myaccount.google.com")){
+                            if(!linksNew[k].href.includes("accounts.google.com")){
+                                if(linksNew[k].innerText !== ""){
+                                    titlesNew.push(linksNew[k]);
+                                    console.log(linksNew[k].innerText);
+                                }
+                            }
+                        }
+                    }
+                }
+ 
+                if (!(titlesNew == titles)) {
+                    for (j = titles.length - 1; j < titlesNew.length; ++j) {
+                        requestHandler(j, titlesNew);
+                    }
+                } else {
+                    // do nothing if no new articles detected
+                }
+            } else {
+                // do nothing on scroll up
+            }
+        });
     }
